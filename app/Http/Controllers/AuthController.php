cc804e0d891614b6b7774a19283d039dbf671b41
Ramdhanll <bscdhani30@gmail.com>
2020-04-19 21:54:06 +0700

    update user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,11 @@ use App\User;
 class AuthController extends Controller
 {
     public function login(Request $request) {
+        $request->validate([
+            'email' => ['required', 'string', 'max:255'],
+            'password' => ['required', 'string'],
+        ]);
+
         $http = new \GuzzleHttp\Client;
         try {
             $response = $http->post(config('services.passport.login_endpoint'), [
@@ -16,7 +21,7 @@ class AuthController extends Controller
                     'grant_type'    => 'password',
                     'client_id'     => config('services.passport.client_id'),
                     'client_secret' => config('services.passport.client_secret'),
-                    'username'      => $request->username,
+                    'username'      => $request->email,
                     'password'      => $request->password
 
                 ]
@@ -24,7 +29,7 @@ class AuthController extends Controller
             return $response->getBody();
         } catch (\GuzzleHttp\Exception\BadResponseException $e) {
             if ($e->getCode() === 400) {
-                return response()->json('Invalid Request. Please enter a username or a password.', $e->getCode());
+                return response()->json('Invalid Request. Please enter a email or a password.', $e->getCode());
             } else if ($e->getCode() === 401 ) {
                 return response()->json('Your credentials are incorrect. Please try again.', $e->getCode());
             }
@@ -38,15 +43,17 @@ class AuthController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
 
-        return User::create([
+        User::create([
             'name' => $request->name,
             'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        return response()->json('successfully', 200);
 
     }
 
